841868f316d37943e6794a0dca8b3d893f113f8a
Osky772 <kowalow.oskar@gmail.com>
2018-12-31 14:06:58 +0100

    functionality of picking elements
    This is functionality for generating, displaing and finiding correct elements.
    It's working.

--- script.js
+++ script.js
@@ -6,9 +6,7 @@
 
 const container = document.getElementById("container");
 const box = document.getElementById("box");
-const toDisplay = 10; // level of hardness
-let counterRandomElements = 0;
-let countFoundElements = 0;
+const toDisplay = 21; // level of hardness (how many random element without element fo find)
 let elementsToFind = []; // array of items to find
 let allAnimals;
 let foundAnimals = [];
@@ -42,12 +40,11 @@ const animals = [
 // Generate 4 random animals to pick up and display on screen
 function randomAnimals() {
     // generate elements
-    for (let i = 0; elementsToFind.length !== 4; i++) {
+    for (let i = 0; elementsToFind.length !== 5; i++) {
         const index = Math.floor(Math.random() * animals.length);
         const animalId = animals[index].id;
         elementsToFind.push(animalId);
     };
-
     // create elements and display on screen
     elementsToFind.forEach(function(el) {
         const found = animals.find(function(animal) {
@@ -80,7 +77,6 @@ function createRandomAnimals() {
     // If elements are loaded on page, then add to every element 'click' event with function of finding correct element
     allAnimals = document.querySelectorAll('.animal');
     allAnimals.forEach(addFindingEvent);
-    console.log('all animals', allAnimals)
 }
 
 function findElement() {
@@ -91,19 +87,20 @@ function findElement() {
      });
     // if you found item then remove it
     if (isFound) {
-        countFoundElements += 1;
-        foundAnimals.push(this);
+        // remove element from array
+        const found = elementsToFind.findIndex(el => el === elementId)
+        elementsToFind.splice(found, 1);
         this.remove();
-        if (foundAnimals.length === elementsToFind.length) {
-            console.log('You are a winner!');
-            console.log(foundAnimals);
+        console.log('to find: ', elementsToFind);
+        // If every element found then you win
+        if (elementsToFind.length === 0) {
+            console.log(`%c WINNER`, `font-size: 3rem; color: darkgreen`);
             allAnimals.forEach(removeFindingEvent);
         } else {
-            console.log('play...');
-            console.log('find right one',foundAnimals);
+            console.log(`%c That's correct element!`, `background: lightgreen;`);
         }
     } else {
-        console.log('wrong');
+        console.log(`%c WRONG!!!`, `background: red`);
     }
 }
 //function for adding event on every displayed element
