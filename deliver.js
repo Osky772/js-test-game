cef7229cb9350470cb0fbb77075c1b95cd45a1cc
Osky772 <kowalow.oskar@gmail.com>
2019-01-05 17:42:12 +0100

    moving car - work in progress

--- deliver.js
+++ deliver.js
@@ -1,8 +1,21 @@
 const body = document.querySelector("body");
 const deliverContainer = document.createElement("div");
-deliverContainer.classList.add("container");
+deliverContainer.classList.add("deliver-map");
 body.prepend(deliverContainer);
 
 const car = document.createElement("div");
 car.classList.add("car");
+car.dataset.px = 'px';
 deliverContainer.prepend(car);
+
+function rideRight() {
+    let position = getComputedStyle(document.documentElement).getPropertyValue(`--positionX`);
+    
+    console.log(position);    
+};
+
+window.addEventListener("keydown", function(e) {
+    if (e.keyCode === 39)  {
+        rideRight();
+    };
+});
