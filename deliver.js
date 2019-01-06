ebd7c262b8e8b5247372ee6ae6463e6d6e877359
Osky772 <kowalow.oskar@gmail.com>
2019-01-06 16:42:30 +0100

    driving car - work in progress

--- deliver.js
+++ deliver.js
@@ -6,20 +6,57 @@ body.prepend(deliverContainer);
 const car = document.createElement("div");
 car.classList.add("car");
 car.dataset.px = 'px';
+const suffix = car.dataset.px;
 deliverContainer.prepend(car);
 
 function rideRight() {
     car.style.transform = "rotate(90deg)";
-    const suffix = car.dataset.px;
     let value = getComputedStyle(document.documentElement).getPropertyValue(`--positionX`);
     // trim value of "px"
     let positionX = value.slice(0, -2);
-    console.log(positionX);
+    // update position with more 80px
     document.documentElement.style.setProperty(`--positionX`, Number(positionX) + 80 + suffix);
+    console.log(getComputedStyle(car).transform);
 };
+
+function rideDown() {
+    car.style.transform = "rotate(180deg)";
+    let value = getComputedStyle(document.documentElement).getPropertyValue(`--positionY`);
+    let positionY = value.slice(0, -2);
+    document.documentElement.style.setProperty(`--positionY`, Number(positionY) + 80 + suffix);
+    console.log(getComputedStyle(car).transform);
+};
+
+function rideLeft() {
+    car.style.transform = "rotate(270deg)";
+    let value = getComputedStyle(document.documentElement).getPropertyValue(`--positionX`);
+    let positionX = value.slice(0, -2);
+    document.documentElement.style.setProperty(`--positionX`, Number(positionX) - 80 + suffix);
+    console.log(getComputedStyle(car).transform);
+};
+
+function rideTop() {
+    // if car is turned left then...
+    let direction = getComputedStyle(car).transform;
+    if (direction === "matrix(-1.83697e-16, -1, 1, -1.83697e-16, 0, 0)") {
+        car.style.transform = "rotate(360deg)";
+    } else {
+        car.style.transform = "rotate(0deg)";
+    }
+    console.log(getComputedStyle(car).transform);
+    let value = getComputedStyle(document.documentElement).getPropertyValue(`--positionY`);
+    let positionY = value.slice(0, -2);
+    document.documentElement.style.setProperty(`--positionY`, Number(positionY) - 80 + suffix);
+}
 
 window.addEventListener("keydown", function(e) {
     if (e.keyCode === 39)  {
         rideRight();
+    } else if (e.keyCode === 40) {
+        rideDown();
+    } else if (e.keyCode === 37) {
+        rideLeft();
+    } else if (e.keyCode === 38) {
+        rideTop();
     }
 });
