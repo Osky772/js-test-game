20c3728a786ae4b1c34d25207c040e46b9e6bcc8
Osky772 <kowalow.oskar@gmail.com>
2019-01-07 12:26:31 +0100

    omit house - car driving

--- deliver.js
+++ deliver.js
@@ -7,65 +7,92 @@ car.dataset.px = 'px';
 const suffix = car.dataset.px;
 deliverContainer.prepend(car);
 
+const home1 = document.querySelector(".home1");
+
 let deg = 90;
 let positionY = 0;
 let positionX = 0;
+let carSizeY = positionY + 80;
+let carSizeX = positionX + 80;
+
+let posY = Number(getComputedStyle(home1).getPropertyValue("top").slice(0, -2));
+let posX = Number(getComputedStyle(home1).getPropertyValue("left").slice(0, -2));
+let width = Number(getComputedStyle(home1).getPropertyValue("width").slice(0, -2));
+let height = Number(getComputedStyle(home1).getPropertyValue("height").slice(0, -2));
+console.log('Y', posY)
+console.log('X', posX)
+console.log('wh', width)
+console.log('hh', height)
+
+/* let sizeX = posX + width;
+let sizeY = posY + height;
+console.log(sizeY);
+console.log(sizeX); */
+
 
 function rideRight() {
     deg = 90;
     car.style.transform = `rotate(${deg}deg)`;
-    let value = getComputedStyle(document.documentElement).getPropertyValue(`--positionX`);
-    // trim value of "px"
-    positionX = Number(value.slice(0, -2));
-    console.log('X', positionX, 'Y', positionY)
+    positionX = Number(getComputedStyle(document.documentElement).getPropertyValue(`--positionX`).slice(0, -2));
     // update position with more 80px
     if (positionX < 1040) {
         positionX += 80;
+        console.log('X', positionX, 'Y', positionY)
         document.documentElement.style.setProperty(`--positionX`, positionX + suffix);
     };
 };
 
 function rideDown() {
-    deg = 180;
-    car.style.transform = `rotate(${deg}deg)`;
-    let value = getComputedStyle(document.documentElement).getPropertyValue(`--positionY`);
-    positionY = Number(value.slice(0, -2));
-    console.log('X', positionX, 'Y', positionY);
-    if (positionY < 640) {
-        positionY += 80;
-        document.documentElement.style.setProperty(`--positionY`, positionY + suffix);
+    carSizeY = positionY + 80;
+    if (Boolean(positionX < width && positionX >= posX && positionY + 80 === height)) {
+        positionY;
+    } else {
+        deg = 180;
+        car.style.transform = `rotate(${deg}deg)`;
+        positionY = Number(getComputedStyle(document.documentElement).getPropertyValue(`--positionY`).slice(0, -2));
+        if (positionY < 640) {
+            positionY += 80;
+            console.log('X', positionX, 'Y', positionY);
+            document.documentElement.style.setProperty(`--positionY`, positionY + suffix);
+        };
     };
 };
 
 function rideLeft() {
-    deg = 270;
-    car.style.transform = `rotate(${deg}deg)`;
-    let value = getComputedStyle(document.documentElement).getPropertyValue(`--positionX`);
-    positionX = Number(value.slice(0, -2));
-    console.log('X', positionX, 'Y', positionY)
-    if (positionX > 0) {
-        positionX -= 80;
-        document.documentElement.style.setProperty(`--positionX`, positionX + suffix);
+    if (Boolean(positionX - 80 < width && positionX - 80 >= posX && positionY  === height)) {
+        positionX;
+    } else {
+        deg = 270;
+        car.style.transform = `rotate(${deg}deg)`;
+        positionX = Number(getComputedStyle(document.documentElement).getPropertyValue(`--positionX`).slice(0, -2));
+        if (positionX > 0) {
+            positionX -= 80;
+            console.log('X', positionX, 'Y', positionY);
+            document.documentElement.style.setProperty(`--positionX`, positionX + suffix);
+        };
     };
 };
 
 function rideTop() {
-    if (deg === 270) {
-        deg = 360;
-        car.style.transform = `rotate(${deg}deg)`
-    } else if (deg === 360) {
-        deg = 360;
-        car.style.transform = `rotate(${deg}deg)`
+    if (Boolean(positionX < width && positionX >= posX && positionY - 80 === height)) {
+        positionY;
     } else {
-        deg = 0;
-        car.style.transform = `rotate(${deg}deg)`
-    };
-    let value = getComputedStyle(document.documentElement).getPropertyValue(`--positionY`);
-    positionY = Number(value.slice(0, -2));
-    console.log('X', positionX, 'Y', positionY);
-    if (positionY > 0) {
-        positionY -= 80;
-        document.documentElement.style.setProperty(`--positionY`, positionY + suffix);
+        if (deg === 270) {
+            deg = 360;
+            car.style.transform = `rotate(${deg}deg)`
+        } else if (deg === 360) {
+            deg = 360;
+            car.style.transform = `rotate(${deg}deg)`
+        } else {
+            deg = 0;
+            car.style.transform = `rotate(${deg}deg)`
+        };
+        positionY = Number(getComputedStyle(document.documentElement).getPropertyValue(`--positionY`).slice(0, -2));
+        if (positionY > 0) {
+            positionY -= 80;
+            console.log('X', positionX, 'Y', positionY);
+            document.documentElement.style.setProperty(`--positionY`, positionY + suffix);
+        };
     };
 };
 
